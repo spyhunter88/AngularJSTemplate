5e8f05db38d5ce8f153894332f39bf23d35d15d8
spyhunter88 <spyhunter8801@yahoo.com.vn>
2014-12-14 10:31:29 +0700

    Add missing file

---
+++ AngularJS.Services/AutoMapperConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AngularJS.Entities.Models;
+using AngularJS.Services.DTO;
+using AutoMapper;
+
+namespace AngularJS.Services
+{
+    public class AutoMapperConfiguration
+    {
+        public static void Configure()
+        {
+            Mapper.CreateMap<Claim, ClaimDTO>();
+            Mapper.CreateMap<Claim, ClaimLiteDTO>();
+            Mapper.CreateMap<CheckPoint, CheckPointDTO>();
+            Mapper.CreateMap<Requirement, RequirementDTO>();
+            // Mapper.CreateMap<Feed, FeedDTO>();
+        }
+    }
+}
