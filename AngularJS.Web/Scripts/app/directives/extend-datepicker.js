466cec6fa93e0fae74ae899746ed0ac1bcc30ad8
spyhunter88 <spyhunter8801@yahoo.com.vn>
2014-12-18 17:59:49 +0700

    Test ext-datepicker #1

--- AngularJS.Web/Scripts/app/directives/extend-datepicker.js
+++ AngularJS.Web/Scripts/app/directives/extend-datepicker.js
@@ -1,6 +1,6 @@
 /*!
  * angular-ext-datepicker - v0.0.1
- * https://github.com/spyhunter88/angular-datatables
+ * https://github.com/spyhunter88/ext-datepicker
  * License: MIT
  */
  
@@ -12,28 +12,34 @@
 	.directive('extDatepicker', function ($compile, ngToast) {
 		return {
 			scope: {
-				open: '='
+			    date: '=ngModel'
 			},
 			restrict: 'A',
 			replace: false,
 			terminal: true, // this setting is important
 			priority: 1000, // this setting is important, bigger come first
-			controller: function ($scope, $element) {
-				$scope.open = function () {
-					$scope.preventDefault();
-					ngToast.create('Appear');
-				};
+			link: function ($scope, elements, attr) {
+			    $scope.openDatePicker = function () {
+			        $scope.isOpen = true;
+			        console.log('Alright!');
+			    };
+
+			    $scope.isOpen = true;
+			    elements.attr('datepicker-popup', 'dd/MM/yyyy');
+			    elements.attr('is-open', 'isOpen');
+			    elements.attr('ng-click', 'isOpen = true;');
+			    elements.removeAttr('ext-datepicker');
+			    // elements.after('<span class="input-group-btn"><button type="button" class="btn btn-default btn-sm" ng-click="openDatePicker"><i class="glyphicon glyphicon-calendar"></i></button>{{isOpen}}</span>');
 			},
-			link: function (scope, element) {
-				scope.open = function () {
-					scope.preventDefault();
-					ngToast.create('Appear');
-				};
-			
-				var datepicker = '<span class="input-group-btn"><button type="button" class="btn btn-default btn-sm" ng-click="open()"><i class="glyphicon glyphicon-calendar"></i></button></span>';
-			
-				$compile(datepicker)(scope);
-				element.append(datepicker);
+			compile: function compile(element, attrs) {
+			    return {
+			        pre: function preLink(scope, iElement, iAttrs, controller) { },
+			        post: function postLink(scope, iElement, iAttrs, controller) {
+			            var append = '<span class="input-group-btn"><button type="button" class="btn btn-default btn-sm" ng-click="openDatePicker"><i class="glyphicon glyphicon-calendar"></i></button>{{isOpen}}</span>';
+			            $compile(append)(scope);
+			            iElement.after(append);
+			        }
+			    };
 			}
 		};
 	});
