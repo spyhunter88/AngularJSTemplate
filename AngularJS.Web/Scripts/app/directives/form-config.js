c229bf08b27faaa03cf6928f16d3656012573e0d
spyhunter8801 <spyhunter8801@yahoo.com.vn>
2015-05-12 17:56:33 +0700

    small fixed in get list of property

--- AngularJS.Web/Scripts/app/directives/form-config.js
+++ AngularJS.Web/Scripts/app/directives/form-config.js
@@ -23,13 +23,24 @@
                     if (scope.config === undefined) return;
                     var config = angular.fromJson(scope.config);
 
+                    var _disabled = _getProperty(config, "disabled");
+
                     angular.forEach(objects, function (obj) {
                         var name = angular.element(obj).attr('name');
-                        if (config.disable.indexOf(name) != -1) {
-                            // angular.element(obj).attr('readonly', true);
+                        if (_disabled.indexOf(name) != -1) {
                             angular.element(obj).attr('disabled', true);
                         }
                     });
+
+
+                    var _getProperty = function (list, property) {
+                        var res = [];
+                        angular.forEach(list, function (obj) {
+                            if (obj.fieldProperty === property) res.push = obj.objectFiled;
+                        });
+
+                        return res;
+                    };
                 });
             }
         };
