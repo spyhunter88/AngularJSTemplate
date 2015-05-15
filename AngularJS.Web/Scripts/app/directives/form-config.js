f8e3a944c39a11a435a6e0517be8f8abb6c987c7
spyhunter8801 <spyhunter8801@yahoo.com.vn>
2015-05-15 17:01:39 +0700

    Change form-config to use ng-show/hide #1

--- AngularJS.Web/Scripts/app/directives/form-config.js
+++ AngularJS.Web/Scripts/app/directives/form-config.js
@@ -14,41 +14,52 @@
     function formConfig($compile) {
         return {
             restrict: 'A',
-            scope: {config: '=formConfig' },
+            scope: {
+                config: '=',
+                read: '='
+            },
             link: function (scope, element, attrs) {
+                // this.readonly = {};
+                var _getProperty = function (list, property) {
+                    var res = [];
+                    angular.forEach(list, function (obj) {
+                        // console.log(obj);
+                        if (obj.FieldProperty == property) res.push(obj.ObjectField);
+                    });
+                    return res;
+                };
+
                 // watch 'config' to listen to config outside
                 scope.$watch('config', function () {
-                    var _getProperty = function (list, property) {
-                        var res = [];
-                        angular.forEach(list, function (obj) {
-                            console.log(obj);
-                            if (obj.FieldProperty == property) res.push(obj.ObjectField);
-                        });
-                        return res;
-                    };
+                    scope.read = {};
 
-                    var objects = element[0].querySelectorAll('input, checkbox, select, textarea');
+                    // var objects = element[0].querySelectorAll('input, checkbox, select, textarea');
                     // console.log(scope.config);
                     if (scope.config === undefined) return;
                     var config = angular.fromJson(scope.config);
 
                     var _disabled = _getProperty(config, "disabled");
 
-                    angular.forEach(objects, function (obj) {
-                        var name = angular.element(obj).attr('name');
-                        if (_disabled.indexOf(name) != -1) {
-                            if (obj.tagName.toLowerCase() == "select") angular.element(obj).attr('disabled', true);
-                            else {
-                                angular.element(obj).attr('readonly', true);
-                                angular.element(obj).attr('ng-value', angular.element(obj).attr('ng-model'));
-                                angular.element(obj).removeAttr('ng-model');
-                            }
+                    angular.forEach(_disabled, function (name) {
+                        scope.read[name] = true;
 
-                            // Re-update directive (i.e.: datepicker ...)
-                            $compile(obj)(scope);
-                        }
+                        //var name = angular.element(obj).attr('name');
+                        //if (_disabled.indexOf(name) != -1) {
+                        //    if (obj.tagName.toLowerCase() == "select") angular.element(obj).attr('disabled', true);
+                        //    else {
+                        //        angular.element(obj).attr('readonly', true);
+                        //        angular.element(obj).attr('ng-value', angular.element(obj).attr('ng-model'));
+                        //        angular.element(obj).removeAttr('ng-model');
+                        //    }
+
+                        //    // Re-update directive (i.e.: datepicker ...)
+                        //    $compile(obj)(scope);
+                        //}
                     });
-                });
+                    console.log(scope.read);
+
+                    // $compile(element)(scope);
+                }, true);
             }
         };
     };
